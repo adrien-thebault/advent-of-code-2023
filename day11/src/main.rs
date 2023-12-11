4563fd82e097a7db482b8857371998300101b24c
Adrien Thébault <adrien@thebault.xyz>
2023-12-11 09:09:39 +0100

    day 11

---
+++ day11/src/main.rs
@@ -0,0 +1,56 @@
+use itertools::Itertools;
+use std::cmp;
+
+fn main() {
+    let input = String::from_utf8_lossy(include_bytes!("input.txt"))
+        .lines()
+        .map(|l| l.chars().collect_vec())
+        .collect_vec();
+
+    let (empty_rows, empty_cols) = (
+        input
+            .iter()
+            .enumerate()
+            .filter_map(|(x, l)| l.iter().all(|c| *c == '.').then_some(x))
+            .collect_vec(),
+        (0..input[0].len())
+            .filter(|y| input.iter().all(|l| l[*y] == '.'))
+            .collect_vec(),
+    );
+
+    let solve = |input: &[_], e: usize| {
+        input
+            .iter()
+            .enumerate()
+            .flat_map(|(x, l): (_, &Vec<_>)| {
+                l.iter()
+                    .enumerate()
+                    .filter_map(move |(y, c)| (*c == '#').then_some((x, y)))
+            })
+            .combinations(2)
+            .map(|s| {
+                let (dx, dy, mx, my) = (
+                    s[0].0.abs_diff(s[1].0),
+                    s[0].1.abs_diff(s[1].1),
+                    cmp::min(s[0].0, s[1].0),
+                    cmp::min(s[0].1, s[1].1),
+                );
+                let empty = empty_rows
+                    .iter()
+                    .filter(|r| **r > mx && **r < mx + dx)
+                    .count()
+                    + empty_cols
+                        .iter()
+                        .filter(|c| **c > my && **c < my + dy)
+                        .count();
+                dx + dy + empty * (e - 1)
+            })
+            .sum::<usize>()
+    };
+
+    // part 1
+    println!("part 1 : {}", solve(&input, 2));
+
+    // part 2
+    println!("part 2 : {}", solve(&input, 1_000_000));
+}
